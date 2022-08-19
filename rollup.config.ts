c52078a706681b9cc1b9217cc11712b6fb7a4161
michalsmiarowski <michal.smiarowski@thesis.co>
2022-08-19 15:57:48 +0200

    Add @rollup/plugin-image
    Add @rollup/plugin-image to make it possible to encode image to base64 and thus
    make it possible to make the import of the image in react and then building the
    package without any problems.

--- rollup.config.ts
+++ rollup.config.ts
@@ -3,6 +3,7 @@ import resolve from "@rollup/plugin-node-resolve"
 import commonjs from "@rollup/plugin-commonjs"
 import typescript from "rollup-plugin-typescript2"
 import postcss from "rollup-plugin-postcss"
+import image from "@rollup/plugin-image"
 
 const packageJson = require("./package.json")
 
@@ -35,5 +36,6 @@ export default {
     postcss({
       extensions: [".css"],
     }),
+    image(),
   ],
 }
