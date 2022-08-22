c1ba9203a7578bcd1847686635b21c9c96cfd904
George Weiler <georgejweiler@gmail.com>
2022-08-22 16:13:22 -0600

    Add extra statuses to the box label

--- src/theme/BoxLabel.ts
+++ src/theme/BoxLabel.ts
@@ -1,8 +1,15 @@
 import { ComponentStyleConfig } from "@chakra-ui/react"
 import { colors } from "./utils"
+
 export type BoxLabelVariant = "solid" | "outline"
 export type BoxLabelSize = "xs" | "sm" | "md" | "lg"
-export type BoxLabelStatus = "primary" | "secondary" | "magic"
+export type BoxLabelStatus =
+  | "primary"
+  | "secondary"
+  | "magic"
+  | "success"
+  | "warning"
+  | "error"
 
 type ColorMap = {
   [key in BoxLabelStatus]: string
@@ -25,12 +32,18 @@ export const BoxLabel: ComponentStyleConfig = {
           primary: "brand.50",
           secondary: "gray.50",
           magic: colors.gradient[3],
+          success: "green.50",
+          warning: "yellow.50",
+          error: "red.50",
         }[status],
         border: "none",
         color: {
           primary: "brand.700",
           secondary: "gray.700",
           magic: "white",
+          success: "green.700",
+          warning: "yellow.700",
+          error: "red.700",
         }[status],
       }
     },
@@ -40,6 +53,9 @@ export const BoxLabel: ComponentStyleConfig = {
         primary: "brand.700",
         secondary: "gray.700",
         magic: "brand.500",
+        success: "green.500",
+        warning: "yellow.500",
+        error: "red.500",
       }
 
       return {
